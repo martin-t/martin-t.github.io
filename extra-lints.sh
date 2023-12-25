428d7c8c3e3d39f929adf168b61334438daab46b
Martin Taibr <taibr.martin@gmail.com>
2023-12-25 14:11:17 +0100

    Fix lints

--- extra-lints.sh
+++ extra-lints.sh
@@ -8,7 +8,7 @@ RET=0
 # Grep returns success when found and failure when not found.
 # The `[F]` is the usual trick to avoid matching this line itself
 # without excluding this whole file so it's still checked.
-if grep --recursive --color=auto --exclude-dir=.git --exclude-dir=target --exclude-dir=screenshots [F]IXME . ; then
+if grep --recursive --color=auto --exclude-dir=.git --exclude-dir=public --exclude-dir=screenshots [F]IXME . ; then
     echo "The lines above this message must be fixed (or marked as todo/later in uppercase, not fixme).
 
 Fixmes are to be fixed before committing or at least before merging to master so they can be used during development for things that must not be forgotten and grep's output is not littered with other people's fixmes."
